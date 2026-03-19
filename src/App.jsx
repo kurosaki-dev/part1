55ab11f24a7601681c5948a82ae70ecbe64c1e21
Kuro <kaelcarreon0930@gmail.com>
2026-03-19 14:16:17 +0800

    1.4: Course Information step 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
@@ -22,9 +24,9 @@ const App = () => {
       <Content part={part3} exercise={exercises3} /> */}
 
       {/* Solution #2: 1.1 */}
-      {/* 1.3 */}
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      {/* 1.4 */}
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -46,10 +48,10 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      {/* 1.3 */}
-      <Part part={props.part1.name} exercise={props.part1.exercises} />
-      <Part part={props.part2.name} exercise={props.part2.exercises} />
-      <Part part={props.part3.name} exercise={props.part3.exercises} />
+      {/* 1.4 */}
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
     </>
   );
 };
@@ -58,7 +60,9 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.part1.exercises + props.part2.exercises + props.part3.exercises}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
     </p>
   );
 };
